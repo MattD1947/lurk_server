ea02efcf6c3e35d9bbaec23240541a16fe868f19
MattD1947 <47840513+MattD1947@users.noreply.github.com>
2021-05-28 09:43:23 -0700

    Update server.py

--- server.py
+++ server.py
@@ -127,22 +127,6 @@ def runningThreading(client_socket, address,characters):
 
     recvMessage(client_socket, characters,address)
 
-#    send_message[str(client_socket)] = threading.Thread(target=sendMessage, args=[client_socket, address])
-#    send_message[str(client_socket)].start()
-
-#   update_message[str(client_socket)] = threading.Thread(target=updateMessage, args=[client_socket,address])
-#    update_message[str(client_socket)].start()
-
-
-    # checking server update (other clients may send message to the others, in that case, the server will need to
-    # response between this two clients)
-
-
-
-
-    # send game description
-
-    # Type = client_socket.recv(1)[0]
 def ServerInput(characters):
 
 	while(True):
@@ -154,9 +138,6 @@ def main():
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 	address = sys.argv[1]
 	port = int(sys.argv[2])
-
-	# server_socket.bind((socket.gethostname(), port)) 
-	# assign this above if you are running this locally
 	server_socket.bind((address, port))
 	server_socket.listen(65535)
 	sockets_list = [server_socket]
@@ -179,121 +160,3 @@ def main():
 			pass
 
 main()
-
-
-# def receive_message(client_socket):
-#     try:
-#         receive_type = client_socket.recv(1)
-
-#         if not len(receive_type):
-#             return False
-
-#         the_type(receive_type)
-
-#         message_length = int(message_header.decode('ascii'.strip()))
-#         return {"header": message_header, "data": client_socket.recv(message_length)}
-#     except:
-#         return False
-
-# while True:
-#     read_sockets, _, exception_sockets = select.select(sockets_list,[],sockets_list)
-#     for notified_socket in read_sockets:
-#         if notified_socket == server_socket:
-#             client_socket, client_address = server_socket.accept()
-
-#             print(f"Accepted new connection from {client_address[0]}:{client_address[1]}")
-	    # user = receive_message(client_socket)
-	    # if user is False:
-	    #     continue
-	    # sockets_list.append(client_socket)
-
-	    # sent server info
-	    # info = pack("<"+"5B",14,2,2,0,0)
-	    # client_socket.sendall(info)
-
-	    #sent game
-	    # Type = 11
-	    # initialPoints = 100
-	    # statLimit = 65535
-	    # description = "Welcome to Matt's server"
-	    # descriptionLength = len(description)
-	    # Format = '<'+'B'+'3H'
-	    # game = pack(Format,Type,initialPoints,statLimit,descriptionLength)
-	    # client_socket.sendall(game)
-
-	    #send game description
-	    # client_socket.sendall(bytes(description,"ascii"))
-
-	    # Type = client_socket.recv(1)[0]
-
-	    #test message
-	    # if Type == 1:
-	    #     data = client_socket.recv(66)
-	    #     lines,size = Message(data)
-	    #     message_description = client_socket.recv(size)
-	    #     message_description_line = MessageDescription(message_description)
-	    #     lines.append(message_description_line)
-	    #     for i in lines:
-	    #         print(i)
-	    # client_socket.sendall(data)
-	    # client_socket.sendall(message_description)
-
-	    #send error
-
-
-
-
-
-
-
-	    # clients[client_socket] = user
-	    # username: {user['data'].decode("ascii")
-
-	# else:
-	    # message = receive_message(notified_socket)
-	    # print(f"Connection lost from {notified_socket.decode('ascii')}")
-	    # sockets_list.remove(notified_socket)
-	    # print(f"Closed connection from {clients[notified_socket].decode('ascii')}")
-
-	    # if message is False:
-	    #     sockets_list.remove(notified_socket)
-	    #     del clients[notified_socket]
-	    #     continue
-	    # user = clients[notified_socket]
-
-	    # print(f"Received message from {user['data'].decode('ascii')} ")
-
-	    # for client_socket in clients:
-	    #     if client_socket != notified_socket:
-	    #         client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
-    # for notified_socket in exception_sockets:
-    #     print(f"Closed connection from {notified_socket}")
-    #     sockets_list.remove(notified_socket)
-    #     del clients[notified_socket]
-    # if len(sockets_list) >= 65535:
-    #     print()
-
-# while True:
-#     read_sockets, _, exception_sockets = select.select(sockets_list,[],sockets_list)
-#     for notified_socket in read_sockets:
-#         notified_socket.connect()
-#         client_socket.send(bytes("Welcome to the server!", "ascii"))
-#         time.sleep(5)
-
-
-# def main():
-#     t1 = threading.Thread(target = TopWin)
-#     t2 = threading.Thread(target = BottomWin)
-#     threads = []
-#     t1.start()
-#     t2.start()
-#     threads.append(t1)
-#     threads.append(t2)
-#     for thread in threads:
-#         thread.join()
-# main()
-
-
-
-
